6a352aee53d20dcd5b2b98afed61f6491fdfbb68
debrunei000 <debrunei000@warren.k12.in.us>
2025-12-12 13:51:47 -0500

    v

--- src/main/java/Shapes.java
+++ src/main/java/Shapes.java
@@ -1,9 +1,16 @@
 import com.almasb.fxgl.app.GameApplication;
 import com.almasb.fxgl.app.GameSettings;
 import com.almasb.fxgl.app.scene.Camera3D;
+import com.almasb.fxgl.dsl.FXGL;
 import com.almasb.fxgl.entity.Entity;
 import com.almasb.fxgl.entity.components.TransformComponent;
+import com.almasb.fxgl.scene3d.Prism;
+import com.almasb.fxgl.scene3d.Torus;
 import javafx.scene.Node;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Box;
+import javafx.scene.shape.Cylinder;
+import javafx.scene.shape.Sphere;
 
 import static com.almasb.fxgl.dsl.FXGLForKtKt.getGameScene;
 
@@ -31,15 +38,39 @@ public class Shapes extends GameApplication {
         transform.translateZ(-10);
         transform.translateY(0);
 
-        getGameScene()
+        getGameScene().setBackgroundColor(Color.LIGHTBLUE);
+        getGameScene().setFPSCamera(true);
+        getGameScene().setCursorInvisible();
+
+        Node[] shapes = makeShapes();
+        int x = 0;
+
+        for (var shape : shapes) {
+            Entity e = makeEntity(x * 2 -8, 0, 6);
+            e.getViewComponent().addChild(shape);
+
+            x++;
+        }
     }
 
     private Node[] makeShapes() {
-        return null; // TODO: replace this with better.
+        return new Node[]{
+            new Box(),
+            new Sphere(),
+            new Torus(),
+            new Prism(1, 0.5, 2, 3),
+            new Prism(),
+            new Prism(1, 1, 2, 4),
+            new Prism(1, 1, 2, 5),
+            new Prism(1, 1, 2, 6),
+            new Cylinder()
+        };
     }
 
     private Entity makeEntity(double x, double y, double z) {
-        return null; // TODO: replace this with better.
+        return FXGL.entityBuilder()
+                .at(x, y, z)
+                .buildAndAttach();
     }
 
 
